127417f014d030f269db5266a89d068a8d8b319e
Gothmog, World Eater <6ixw92+ekwq3eak4sp4o@sharklasers.com>
2018-04-30 18:19:51 -0400

    Added the main page

---
+++ index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+	<title></title>
+	<meta name="generator" content="LibreOffice 6.0.3.2 (Linux)"/>
+	<meta name="created" content="2018-04-30T17:38:52.297608380"/>
+	<meta name="changed" content="2018-04-30T17:55:52.707724199"/>
+	<style type="text/css">
+		td p.cjk { font-size: 10pt }
+	</style>
+</head>
+<body lang="en-US" dir="ltr">
+<p align="center" style="background: #00aaad"><font face="Ubuntu"><font size="7" style="font-size: 60pt"><u>Welcome!</u></font></font></p>
+<p align="center"><font face="Ubuntu"><font size="6" style="font-size: 28pt"><span style="text-decoration: none">Thank
+you for visiting! This website has a number of purposes, including an
+anonymous chatroom, programming forum download hoster, and a meme
+page! Feel free to look around as you wish!</span></font></font></p>
+<table width="100%" cellpadding="0" cellspacing="0" style="page-break-after: auto; page-break-inside: auto">
+	<col width="200*">
+	<col width="56*">
+	<tr valign="top">
+		<td width="78%" height="77" bgcolor="#009353" style="background: #009353" style="border-top: 1px solid #009353; border-bottom: none; border-left: 1px solid #009353; border-right: none; padding: 0in" sdnum="1033;0;@">
+			<p class="western" align="center" style="font-style: normal; text-decoration: none">
+			<br/>
+<br/>
+
+			</p>
+			<p class="western" align="center" style="font-style: normal; text-decoration: none">
+			<font color="#ffffff"><font face="Liberation Sans, sans-serif"><font size="3" style="font-size: 12pt"><b><span style="text-decoration: none">PAGE
+			INDEX</span></b></font></font></font></p>
+			<p class="western" align="left" style="font-style: normal; text-decoration: none">
+			<br/>
+
+			</p>
+		</td>
+		<td width="22%" bgcolor="#009353" style="background: #009353" style="border-top: 1px solid #009353; border-bottom: none; border-left: 1px solid #009353; border-right: 1px solid #009353; padding: 0in" sdnum="1033;0;@">
+			<p class="western" align="center" style="font-style: normal; text-decoration: none">
+			<br/>
+<br/>
+
+			</p>
+			<p class="western" align="center" style="font-style: normal; text-decoration: none">
+			<font color="#ffffff"><font face="Liberation Sans, sans-serif"><font size="3" style="font-size: 12pt"><b><span style="text-decoration: none">NEWS
+			AND ANNOUNCEMENTS</span></b></font></font></font></p>
+		</td>
+	</tr>
+	<tr valign="top">
+		<td width="78%" height="174" bgcolor="#bee3d3" style="background: #bee3d3" style="border-top: none; border-bottom: 1px solid #009353; border-left: 1px solid #009353; border-right: none; padding: 0in" sdnum="1033;0;@">
+			<table width="100%" cellpadding="4" cellspacing="0" style="page-break-inside: avoid">
+				<col width="128*">
+				<col width="128*">
+				<tr valign="top">
+					<td width="50%" style="border-top: 1px double #808080; border-bottom: 1px double #808080; border-left: 1px double #808080; border-right: none; padding-top: 0.04in; padding-bottom: 0.04in; padding-left: 0.04in; padding-right: 0in">
+						<p class="western" align="center"><font face="Liberation Sans, sans-serif"><font size="3" style="font-size: 12pt">LINK</font></font></p>
+					</td>
+					<td width="50%" style="border: 1px double #808080; padding: 0.04in">
+						<p class="western" align="center"><font face="Liberation Sans, sans-serif"><font size="3" style="font-size: 12pt">DESCRIPTION</font></font></p>
+					</td>
+				</tr>
+				<tr valign="top">
+					<td width="50%" style="border-top: none; border-bottom: 1px double #808080; border-left: 1px double #808080; border-right: none; padding-top: 0in; padding-bottom: 0.04in; padding-left: 0.04in; padding-right: 0in">
+						<p class="western" align="center"><br/>
+
+						</p>
+					</td>
+					<td width="50%" style="border-top: none; border-bottom: 1px double #808080; border-left: 1px double #808080; border-right: 1px double #808080; padding-top: 0in; padding-bottom: 0.04in; padding-left: 0.04in; padding-right: 0.04in">
+						<p class="western" align="center"><font face="Liberation Sans, sans-serif"><font size="3" style="font-size: 12pt">An
+						anonymous, uncensored text based chat interface. Does not
+						require login.</font></font></p>
+					</td>
+				</tr>
+				<tr valign="top">
+					<td width="50%" style="border-top: none; border-bottom: 1px double #808080; border-left: 1px double #808080; border-right: none; padding-top: 0in; padding-bottom: 0.04in; padding-left: 0.04in; padding-right: 0in">
+						<p class="western" align="center"><br/>
+
+						</p>
+					</td>
+					<td width="50%" style="border-top: none; border-bottom: 1px double #808080; border-left: 1px double #808080; border-right: 1px double #808080; padding-top: 0in; padding-bottom: 0.04in; padding-left: 0.04in; padding-right: 0.04in">
+						<p class="western" align="center"><font face="Liberation Sans, sans-serif"><font size="3" style="font-size: 12pt">A
+						simple programming forum board</font></font></p>
+					</td>
+				</tr>
+				<tr valign="top">
+					<td width="50%" style="border-top: none; border-bottom: 1px double #808080; border-left: 1px double #808080; border-right: none; padding-top: 0in; padding-bottom: 0.04in; padding-left: 0.04in; padding-right: 0in">
+						<p class="western" align="center"><br/>
+
+						</p>
+					</td>
+					<td width="50%" style="border-top: none; border-bottom: 1px double #808080; border-left: 1px double #808080; border-right: 1px double #808080; padding-top: 0in; padding-bottom: 0.04in; padding-left: 0.04in; padding-right: 0.04in">
+						<p class="western" align="center"><font face="Liberation Sans, sans-serif"><font size="3" style="font-size: 12pt">Blogs
+						from the anonymous founder of this site.</font></font></p>
+					</td>
+				</tr>
+				<tr valign="top">
+					<td width="50%" style="border-top: none; border-bottom: 1px double #808080; border-left: 1px double #808080; border-right: none; padding-top: 0in; padding-bottom: 0.04in; padding-left: 0.04in; padding-right: 0in">
+						<p class="western" align="center"><br/>
+
+						</p>
+					</td>
+					<td width="50%" style="border-top: none; border-bottom: 1px double #808080; border-left: 1px double #808080; border-right: 1px double #808080; padding-top: 0in; padding-bottom: 0.04in; padding-left: 0.04in; padding-right: 0.04in">
+						<p class="western" align="center"><font face="Liberation Sans, sans-serif"><font size="3" style="font-size: 12pt">A
+						dank-ass meme board</font></font></p>
+					</td>
+				</tr>
+				<tr valign="top">
+					<td width="50%" style="border-top: none; border-bottom: 1px double #808080; border-left: 1px double #808080; border-right: none; padding-top: 0in; padding-bottom: 0.04in; padding-left: 0.04in; padding-right: 0in">
+						<p class="western" align="center"><br/>
+
+						</p>
+					</td>
+					<td width="50%" style="border-top: none; border-bottom: 1px double #808080; border-left: 1px double #808080; border-right: 1px double #808080; padding-top: 0in; padding-bottom: 0.04in; padding-left: 0.04in; padding-right: 0.04in">
+						<p class="western" align="center"><font face="Liberation Sans, sans-serif"><font size="3" style="font-size: 12pt">Help
+						and information about this website</font></font></p>
+					</td>
+				</tr>
+			</table>
+			<p class="western" align="center" style="font-style: normal; text-decoration: none">
+			<br/>
+<br/>
+
+			</p>
+		</td>
+		<td width="22%" bgcolor="#bee3d3" style="background: #bee3d3" style="border-top: none; border-bottom: 1px solid #009353; border-left: 1px solid #009353; border-right: 1px solid #009353; padding: 0in">
+			<p class="western" align="left" style="border-top: none; border-bottom: 1px solid #000000; border-left: none; border-right: none; padding-top: 0in; padding-bottom: 0.03in; padding-left: 0in; padding-right: 0in; font-style: normal; text-decoration: none">
+			<br/>
+<br/>
+
+			</p>
+			<p class="western" align="left" style="border-top: none; border-bottom: 1px solid #000000; border-left: none; border-right: none; padding-top: 0in; padding-bottom: 0.03in; padding-left: 0in; padding-right: 0in; font-style: normal; text-decoration: none">
+			<font color="#000000"><font face="Liberation Sans, sans-serif"><font size="3" style="font-size: 12pt"><b><span style="text-decoration: none">April
+			30<sup>th</sup> 2018 : This site was created.</span></b></font></font></font></p>
+			<p class="western" align="left" style="border-top: none; border-bottom: 1px solid #000000; border-left: none; border-right: none; padding-top: 0in; padding-bottom: 0.03in; padding-left: 0in; padding-right: 0in; font-style: normal; text-decoration: none">
+			<br/>
+<br/>
+
+			</p>
+			<p class="western" align="left" style="font-style: normal; text-decoration: none">
+			<br/>
+<br/>
+
+			</p>
+		</td>
+	</tr>
+</table>
+<p align="left"><br/>
+<br/>
+
+</p>
+</body>
+</html>
