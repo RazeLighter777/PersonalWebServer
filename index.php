350a38d0aae2571564479a1f32b14531433d69db
Justin Suess <justinsuess@sbcglobal.net>
2018-05-01 18:17:40 -0400

    Adjusted layout text

--- index.php
+++ index.php
@@ -3,24 +3,24 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" type="text/css" href="style.css" />
-<title>NightBeach | florida web design</title>
+<title>Welcome to SLACKBOARD</title>
 </head>
 
 <body>
 <div id="container">
 		<div id="header">
         	<h1>Slick<span class="off">Blue</span></h1>
-            <h2>A template by Bryant Smith</h2>
+            <h2>Internet Hangout</h2>
         </div>   
         
         <div id="menu">
         	<ul>
             	<li class="menuitem"><a href="#">Home</a></li>
-                <li class="menuitem"><a href="#">About</a></li>
-                <li class="menuitem"><a href="#">Products</a></li>
-                <li class="menuitem"><a href="#">Services</a></li>
-                <li class="menuitem"><a href="#">Design</a></li>
-              <li class="menuitem"><a href="#">Contact</a></li>
+                <li class="menuitem"><a href="#">Forum</a></li>
+                <li class="menuitem"><a href="#">Live Chat</a></li>
+                <li class="menuitem"><a href="#">Memes</a></li>
+                <li class="menuitem"><a href="#">Downloads</a></li>
+              <li class="menuitem"><a href="#">Information</a></li>
             </ul>
         </div>
         
@@ -30,18 +30,13 @@
 
 				<div id="leftmenu_main">    
                 
-                <h3>Links</h3>
+                <h3>Cool Sites</h3>
                         
                 <ul>
-                    <li><a href="#">SEO</a></li>
-                    <li><a href="#">PHP</a></li>
-                    <li><a href="#">Ajax</a></li>
-                    <li><a href="#">jQuery</a></li>
-                    <li><a href="#">Web design</a></li>
-                    <li><a href="#">Web Programming</a></li>
-                    <li><a href="#">Content Creation</a></li>
-                    <li><a href="#">Internet Marketing</a></li>
-                    <li><a href="#">XHTML Templates</a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#">Library Genisis</a></li>
+                    <li><a href="#">/r/dankmemes</a></li>
+                    <li><a href="#">/r/deepfriedmemes</a></li>
                 </ul>
 </div>
                 
@@ -57,22 +52,22 @@
         
         <div id="content_top"></div>
         <div id="content_main">
-        	<h2>You may use this template in any manner you like. All I ask is that you leave the link back to my site at the bottom of the page. </h2>
+        	<h2>Welcome to SLACKBOARD! </h2>
         	<p>&nbsp;</p>
            	<p>&nbsp;</p>
-       	  <h3>Template Notes</h3>
-        	<p>The main image can be changed by either replacing the current image with another one of the same size (900x402), or using a new one of what ever dimensions you'd like.  If you choose the latter, you must open up style.css and change the dimensions of #mainpic, as well as the file name if that is different. If you would like to move the heading around in the above image, find &quot;#mainpic h1&quot; in style.css and modify it's &quot;left&quot; and &quot;top&quot; properties, this is also true for the h2 tag.</p>
+       	  <h3>What the heck is this site?</h3>
+        	<p>This site serves a number of purposes, including a forum, meme page, anonymous chat, and blog. Feel free to look around!</p>
         	<p>&nbsp;</p>
-<h3>More information</h3>
-        	<p>I decided to leave the content portion open for the templates users to do as they wish with a blank canvas. I don't like to restrict my users too much, and for this reason I leave the defining of any content related styles to you.</p>
+<h3>How can I get started?</h3>
+        	<p>Just click on one of the links. You can look around or talk in the chat room, but if you want to post memes or go on the forum you need an account.</p>
         	<p>&nbsp;</p>
-        	<h3>Template Notes</h3>
-            <p>The main image can be changed by either replacing the current image with another one of the same size (900x402), or using a new one of what ever dimensions you'd like.  If you choose the latter, you must open up style.css and change the dimensions of #mainpic, as well as the file name if that is different. If you would like to move the heading around in the above image, find &quot;#mainpic h1&quot; in style.css and modify it's &quot;left&quot; and &quot;top&quot; properties, this is also true for the h2 tag.</p>
+        	<h3>Infomation</h3>
+            <p>This site is hosted and created by a high school student. I won't put out any contact info, so if you have a problem with me, you're screwed. Otherwise, just feel free to hang out!</p>
             <p>&nbsp;</p>
 </div>
         <div id="content_bottom"></div>
             
-            <div id="footer"><h3><a href="http://www.bryantsmith.com">florida web design</a></h3></div>
+            <div id="footer"><h3><a href="http://www.bryantsmith.com">Credit for web template!</a></h3></div>
       </div>
    </div>
 </body>
