947309808836ea182dfe15de8f0df7222d49a59d
Gothmog, World Eater <6ixw92+ekwq3eak4sp4o@sharklasers.com>
2018-05-02 15:15:59 -0400

    Update handlechat.php

--- handlechat.php
+++ handlechat.php
@@ -1,6 +1,6 @@
 
 <?php
-$captchaSecretCode"6LcizFYUAAAAAKxkxYwQa8QF7F5IAsvZ_XoDjUEI";
+$captchaSecretCode="6LcizFYUAAAAAKxkxYwQa8QF7F5IAsvZ_XoDjUEI";
 
 $response = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$captchaSecretCode."&response=".$_POST['g-recaptcha-response']."&remoteip=".$_SERVER['REMOTE_ADDR']), true);
 
