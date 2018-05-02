8aaea64817f894baa8f496445798cc0fafa7414e
Gothmog, World Eater <6ixw92+ekwq3eak4sp4o@sharklasers.com>
2018-05-02 15:04:46 -0400

    Update handlechat.php

--- handlechat.php
+++ handlechat.php
@@ -1,21 +1,13 @@
 
 <?php
-$secretKey="6LcizFYUAAAAAKxkxYwQa8QF7F5IAsvZ_XoDjUEI";
-if(isset($_POST['g-recaptcha-response'])){
-          $captcha=$_POST['g-recaptcha-response'];
-          //print($captcha);
-        }
-        if(!$captcha){
-          echo '<h2>Please check the the captcha form.</h2>';
-          exit;
-        }
-        $secretKey = "Put your secret key here";
-        $ip = $_SERVER['REMOTE_ADDR'];
-        $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
-        $responseKeys = json_decode($response,true);
-        if(intval($responseKeys["success"]) !== 1) {
-          echo "failed";
-        } else {
-          echo '<h2>Thanks for posting comment.</h2>';
-        }
+$captchaSecretCode"6LcizFYUAAAAAKxkxYwQa8QF7F5IAsvZ_XoDjUEI";
+
+$response = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$captchaSecretCode."&response=".$_POST['g-recaptcha-response']."&remoteip=".$_SERVER['REMOTE_ADDR']), true);
+
+if($response['success'] == true)
+{
+    echo 'true';
+}else{
+    echo 'false';
+}
 ?>
