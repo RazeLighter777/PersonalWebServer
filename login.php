2d4108d007b20bc39954ebe2aa23c807d7f3717a
Justin Suess <justinsuess@sbcglobal.net>
2018-05-04 08:06:56 -0400

    adding login form

---
+++ login.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="style.css" />
+<title>Welcome to SLACKBOARD</title>
+</head>
+
+<body>
+<div id="container">
+		<div id="header">
+		<h1>Slack<span class="off">Board</span></h1>
+	<br>
+            <marquee><h2>Internet Hangout</h2></marquee>
+        </div>   
+        
+        <div id="menu">
+        	<ul>
+		<li class="menuitem"><a href="index.php">Home</a></li>
+		<li class="menuitem"><a href="games.php">Games</a></li>
+                <li class="menuitem"><a href="forum.php">Forum</a></li>
+                <li class="menuitem"><a href="chat.php">Live Chat</a></li>
+                <li class="menuitem"><a href="memes.php">Memes</a></li>
+                <li class="menuitem"><a href="downloads.php">Downloads</a></li>
+              <li class="menuitem"><a href="information.php">Information</a></li>
+            </ul>
+        </div>
+        
+        <div id="leftmenu">
+
+        <div id="leftmenu_top"></div>
+
+				<div id="leftmenu_main">    
+                
+                <h3>Cool Sites</h3>
+                <ul>
+                    <li><a href="http://gen.lib.rus.ec/">Library Genisis</a></li>
+                    <li><a href="http://reddit.com/r/dankmemes">/r/dankmemes</a></li>
+                    <li><a href="http://reddit.com/r/deepfriedmemes">/r/deepfriedmemes</a></li>
+                </ul>
+</div>
+                
+                
+              <div id="leftmenu_bottom"></div>
+        </div>
+        
+        
+        
+        
+		<div id="content">
+        
+        
+        <div id="content_top"></div>
+        <div id="content_main">
+		<form action="handlelogin.php" method="post">
+		<label  for="username">
+		<h2>ENTER YOUR USERNAME</h2>
+		<input type="text" name="username" id = "username" placeholder="ENTER USERNAME">
+		<br><br>
+		<h2>ENTER YOUR PASSWORD</h2>
+		<label for="password">
+		<input type="password" name="passwd" id="passwd" placeholder="ENTER PASSWORD">
+		<br><br>
+		</form>
+	</div>
+        <div id="content_bottom"></div>
+            
+            <div id="footer"><h3><a href="http://www.bryantsmith.com">Credit for web template!</a></h3></div>
+      </div>
+   </div>
+</body>
+</html>
